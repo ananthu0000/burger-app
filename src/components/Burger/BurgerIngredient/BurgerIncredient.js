69110a03c4334d5b3917026f869a4952db779002
ananthu <ananthu0000@gmail.com>
2019-12-24 16:39:18 +0530

    basic setup, initial commit

---
+++ src/components/Burger/BurgerIngredient/BurgerIncredient.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classes from './BurgerIncredient.css';
+
+class BurgerIncredient extends Component {
+    render () {
+        let incredient = null;
+        switch (this.props.type) {
+            case('bread-bottom'):
+                incredient = <div className={classes.BreadBottom}></div>;
+                break;
+
+            case('bread-top'):
+            incredient = (
+            <div className={classes.BreadTop}>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>
+            </div>
+            );
+            break;
+
+            case('meat'):
+            incredient = <div className={classes.Meat}></div>;
+            break;
+
+            case('cheese'):
+            incredient = <div className={classes.Cheese}></div>;
+            break;
+
+            case('salad'):
+            incredient = <div className={classes.Salad}></div>;
+            break;
+
+            case('becon'):
+            incredient = <div className={classes.Becon}></div>;
+            break;
+
+            default:
+                incredient = null;
+        }
+
+    return incredient;
+    }
+}
+
+BurgerIncredient.PropTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default BurgerIncredient;
